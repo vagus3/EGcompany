1ba05b3240489d67a0331d5cc577101787692f5f
vagus3 <mohoham3@kangnam.ac.kr>
2026-07-08 12:54:06 +0900

    feature: vercel insight 통계 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -34,6 +35,7 @@ export default function RootLayout({
       <body className="flex min-h-full flex-col">
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
